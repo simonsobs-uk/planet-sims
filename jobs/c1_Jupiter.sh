d3f2f64f949ddc5c8d5859934635f4bc93055ed1
Kolen Cheung <christian.kolen@gmail.com>
2023-07-21 20:58:53 +0100

    cleanup

--- jobs/c1_Jupiter.sh
+++ jobs/c1_Jupiter.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 
-# helpers ##############################################################
-
-COLUMNS=72
-
-print_double_line() {
-	eval printf %.0s= '{1..'"${COLUMNS}"\}
-	echo
-}
-
-print_line() {
-	eval printf %.0s- '{1..'"${COLUMNS}"\}
-	echo
-}
-
-########################################################################
-print_double_line
-echo "Unpacking environment..."
+echo '========================================================================'
+echo 'Unpacking environment...'
 tar -xzf pmpm-20230718-Linux-x86_64-OpenMPI.tar.gz -C /tmp
 . /tmp/pmpm-20230718/bin/activate /tmp/pmpm-20230718
-print_line
-echo "Environment is available at:"
+echo '------------------------------------------------------------------------'
+echo 'Environment is available at:'
 which python
 
-print_double_line
-echo "Running run_planet_sims_pipeline.py..."
-./run_planet_sims_pipeline.py c1 Jupiter
+echo '========================================================================'
+echo 'Running run_planet_sims_pipeline.py...'
+command time -v ./run_planet_sims_pipeline.py c1 Jupiter
