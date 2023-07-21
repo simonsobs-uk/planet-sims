d1c9af874f54516bf7458e881ac2b3aa6ccbecaf
Kolen Cheung <christian.kolen@gmail.com>
2023-07-21 20:27:57 +0100

    format

--- demo/demo-lat/map_context.py
+++ demo/demo-lat/map_context.py
@@ -1,68 +1,60 @@
 #!/usr/bin/env python
 
-import os, sys
+import sys
 
-import numpy as np
-import pylab as pl
-import h5py
-
-from sotodlib import core, coords
-import so3g
-from so3g.proj import quat
 from pixell import enplot
+from sotodlib import coords, core
 
-
-ctx = core.Context('context/context.yaml')
+ctx = core.Context("context/context.yaml")
 if len(sys.argv) <= 1:
     for obs in ctx.obsdb.get():
-        print(obs['obs_id'])
+        print(obs["obs_id"])
     sys.exit(0)
 
 for obs_id in sys.argv[1:]:
     this_obs = ctx.obsdb.get(obs_id)
-    for wafer_slot in this_obs['wafer_slots'].split(','):
+    for wafer_slot in this_obs["wafer_slots"].split(","):
         print(obs_id, wafer_slot)
-    
-        tod = ctx.get_obs(obs_id, dets={'dets:wafer_slot': wafer_slot})
-        center_on = tod.obs_info['target']
-    
-        if tod.obs_info.telescope == 'LAT':
-          res, size = .005 * coords.DEG, 3 * coords.DEG
+
+        tod = ctx.get_obs(obs_id, dets={"dets:wafer_slot": wafer_slot})
+        center_on = tod.obs_info["target"]
+
+        if tod.obs_info.telescope == "LAT":
+            res, size = 0.005 * coords.DEG, 3 * coords.DEG
         else:
-          # SAT!
-          res, size = .02 * coords.DEG, 6 * coords.DEG
-    
-    
-        with coords.Timer('compute_source_flags'):
+            # SAT!
+            res, size = 0.02 * coords.DEG, 6 * coords.DEG
+
+        with coords.Timer("compute_source_flags"):
             source_flags = coords.planets.compute_source_flags(
                 tod=tod,
                 center_on=center_on,
                 max_pix=6e6,
-                mask={'shape': 'circle', 'xyr': (0., 0., 0.1)},
+                mask={"shape": "circle", "xyr": (0.0, 0.0, 0.1)},
             )
-    
-        with coords.Timer('make_map call'):
+
+        with coords.Timer("make_map call"):
             maps = coords.planets.make_map(
                 tod,
-                #signal=signal_deconv,
+                # signal=signal_deconv,
                 center_on=center_on,
                 scan_coords=False,
-                thread_algo='domdir', #args.thread_algo,
-                res=res, size=size,
-                comps='T', #args.comps,
-                #filename=args.map_name,
+                thread_algo="domdir",  # args.thread_algo,
+                res=res,
+                size=size,
+                comps="T",  # args.comps,
+                # filename=args.map_name,
                 source_flags=source_flags,
-                #cuts=glitch_flags,
-                n_modes='all',
-                low_pass=1.,
-                info={'obs_id': 'test'}) #args.obs_id})
-    
-        #with coords.Timer('Saving'):
+                # cuts=glitch_flags,
+                n_modes="all",
+                low_pass=1.0,
+                info={"obs_id": "test"},
+            )  # args.obs_id})
+
+        # with coords.Timer('Saving'):
         #    maps['solved'].write('map.fits')
-    
-        with coords.Timer('Plotting'):
+
+        with coords.Timer("Plotting"):
             out_png = f"{obs_id}-{wafer_slot}-ctx.png"
-            p, = enplot.plot(maps['solved'], grid=0, tile=-1)
+            (p,) = enplot.plot(maps["solved"], grid=0, tile=-1)
             enplot.write(out_png, p)
-    
-    
--- demo/demo-lat/write_context.py
+++ demo/demo-lat/write_context.py
@@ -14,28 +14,30 @@ Invoke like this:
 argument.)
 
 """
-from sotodlib.core import metadata
-from so3g.proj import quat
-from sotodlib.io.metadata import write_dataset, read_dataset
-import sotodlib.toast as sotoast
+import glob
+import os
 
-import numpy as np
 import h5py
+import numpy as np
+import sotodlib.toast as sotoast
 import yaml
-
-import os, sys, glob, time
+from so3g.proj import quat
+from sotodlib.core import metadata
+from sotodlib.io.metadata import read_dataset, write_dataset
 
 WAFER_CACHE = {}
+
+
 def get_wafer_info(telescope, cache_file=None):
     if telescope not in WAFER_CACHE and cache_file is not None:
         try:
             WAFER_CACHE[telescope] = read_dataset(cache_file, telescope)
         except:
-            print('Failed to load from cache file.')
+            print("Failed to load from cache file.")
     if telescope not in WAFER_CACHE:
         focalplane = sotoast.SOFocalplane(telescope=telescope)
         # Reduced info ...
-        subkeys = ['band', 'tube_slot', 'wafer_slot']
+        subkeys = ["band", "tube_slot", "wafer_slot"]
         subitems = set()
         for k in focalplane.keys():
             subitems.add(tuple([focalplane[k][_k] for _k in subkeys]))
@@ -46,259 +48,291 @@ def get_wafer_info(telescope, cache_file=None):
             write_dataset(wafers, cache_file, telescope)
     return WAFER_CACHE[telescope]
 
-tube_types = {'f030': 'LF',
-              'f040': 'LF',
-              'f090': 'MF',
-              'f150': 'MF',
-              'f230': 'UHF',
-              'f290': 'UHF',
+
+tube_types = {
+    "f030": "LF",
+    "f040": "LF",
+    "f090": "MF",
+    "f150": "MF",
+    "f230": "UHF",
+    "f290": "UHF",
 }
 
+
 def guess_tube(telescope, wafers):
-    if telescope == 'SAT':
-        for t in ['SAT1', 'SAT2', 'SAT3', 'SAT4']:
+    if telescope == "SAT":
+        for t in ["SAT1", "SAT2", "SAT3", "SAT4"]:
             try:
                 return guess_tube(t, wafers)
             except AssertionError:
                 pass
-        assert(False)  # Did not found wafer in any SAT?
+        assert False  # Did not found wafer in any SAT?
 
-    info = get_wafer_info(telescope, 'telcache')
+    info = get_wafer_info(telescope, "telcache")
     s = np.zeros(len(info), bool)
     for w in wafers:
-        s += info['wafer_slot'] == w
+        s += info["wafer_slot"] == w
 
-    info = info.subset(keys=['tube_slot', 'wafer_slot'], rows=s).distinct()
-    assert(len(set(info['tube_slot'])) == 1)  # only one tube_slot!
+    info = info.subset(keys=["tube_slot", "wafer_slot"], rows=s).distinct()
+    assert len(set(info["tube_slot"])) == 1  # only one tube_slot!
 
-    slot_mask = ''
-    for w in sorted(list(set(info['wafer_slot']))):
-        slot_mask += '1' if w in wafers else '0'
+    slot_mask = ""
+    for w in sorted(list(set(info["wafer_slot"]))):
+        slot_mask += "1" if w in wafers else "0"
 
-    return telescope, info['tube_slot'][0], slot_mask
+    return telescope, info["tube_slot"][0], slot_mask
+
 
 def extract_detdb(hg, db=None):
     if db is None:
         db = metadata.DetDb()
-        db.create_table('base', [
-            "`det_id_` text",  # we can't use "det_id"; rename later
-            "`readout_id` text",
-            "`wafer_slot` text",
-            "`special_ID` text",
-            "`tel_type` text",
-            "`tube_type` text",
-            "`band` text",
-            "`fcode` text",
-            "`toast_band` text",
-            ])
-        db.create_table('quat', [
-            "`r` float",
-            "`i` float",
-            "`j` float",
-            "`k` float",
-            ])
+        db.create_table(
+            "base",
+            [
+                "`det_id_` text",  # we can't use "det_id"; rename later
+                "`readout_id` text",
+                "`wafer_slot` text",
+                "`special_ID` text",
+                "`tel_type` text",
+                "`tube_type` text",
+                "`band` text",
+                "`fcode` text",
+                "`toast_band` text",
+            ],
+        )
+        db.create_table(
+            "quat",
+            [
+                "`r` float",
+                "`i` float",
+                "`j` float",
+                "`k` float",
+            ],
+        )
 
-    existing = list(db.dets()['name'])
+    existing = list(db.dets()["name"])
 
-    tel_type = hg['instrument'].attrs.get('telescope_name')
-    if tel_type in ['LAT', 'SAT']:
+    tel_type = hg["instrument"].attrs.get("telescope_name")
+    if tel_type in ["LAT", "SAT"]:
         pass
     else:
         # new toast has TELn_TUBE
-        tel_type = tel_type.split('_')[0][:3]
-    assert tel_type in ['LAT', 'SAT']
+        tel_type = tel_type.split("_")[0][:3]
+    assert tel_type in ["LAT", "SAT"]
 
-    fp = hg['instrument']['focalplane']
+    fp = hg["instrument"]["focalplane"]
     for dv in fp:
-        v = dict([(_k, dv[_k].decode('ascii'))
-                  for _k in ['wafer_slot', 'band', 'name']])
-        k = v.pop('name')
+        v = dict(
+            [(_k, dv[_k].decode("ascii")) for _k in ["wafer_slot", "band", "name"]]
+        )
+        k = v.pop("name")
         if k in existing:
             continue
-        v['special_ID'] = int(dv['uid'])
-        v['toast_band'] = v['band']
-        v['band'] = v['toast_band'].split('_')[1]
-        v['fcode'] = v['band']
-        v['tel_type'] = tel_type
-        v['tube_type'] = tube_types[v['band']]
-        v['det_id_'] = 'DET_' + k
-        v['readout_id'] = k
-        db.add_props('base', k, **v, commit=False)
-        db.add_props('quat', k, **{'r': dv['quat'][3],
-                                   'i': dv['quat'][0],
-                                   'j': dv['quat'][1],
-                                   'k': dv['quat'][2]})
+        v["special_ID"] = int(dv["uid"])
+        v["toast_band"] = v["band"]
+        v["band"] = v["toast_band"].split("_")[1]
+        v["fcode"] = v["band"]
+        v["tel_type"] = tel_type
+        v["tube_type"] = tube_types[v["band"]]
+        v["det_id_"] = "DET_" + k
+        v["readout_id"] = k
+        db.add_props("base", k, **v, commit=False)
+        db.add_props(
+            "quat",
+            k,
+            **{
+                "r": dv["quat"][3],
+                "i": dv["quat"][0],
+                "j": dv["quat"][1],
+                "k": dv["quat"][2],
+            },
+        )
 
     db.conn.commit()
     db.validate()
     return db
 
+
 def extract_obs_info(h):
-    t = np.asarray(h['shared']['times'])[[0,-1]]
+    t = np.asarray(h["shared"]["times"])[[0, -1]]
     data = {
-        'toast_obs_name': h.attrs['observation_name'],
-        'toast_obs_uid': int(h.attrs['observation_uid']),
-        'target': h.attrs['observation_name'].split('-')[0].lower(),
-        'start_time': t[0],
-        'stop_time': t[1],
-        'timestamp': t[0],
-        'duration': t[1] - t[0],
+        "toast_obs_name": h.attrs["observation_name"],
+        "toast_obs_uid": int(h.attrs["observation_uid"]),
+        "target": h.attrs["observation_name"].split("-")[0].lower(),
+        "start_time": t[0],
+        "stop_time": t[1],
+        "timestamp": t[0],
+        "duration": t[1] - t[0],
     }
     return data
 
 
 def detdb_to_focalplane(db):
     # Focalplane compatible with, like, planet mapper.
-    fp = metadata.ResultSet(keys=['dets:readout_id', 'xi', 'eta', 'gamma'])
-    for row in db.props(props=['readout_id', 'quat.r', 'quat.i', 'quat.j', 'quat.k']).rows:
+    fp = metadata.ResultSet(keys=["dets:readout_id", "xi", "eta", "gamma"])
+    for row in db.props(
+        props=["readout_id", "quat.r", "quat.i", "quat.j", "quat.k"]
+    ).rows:
         q = quat.quat(*row[1:])
         xi, eta, gamma = quat.decompose_xieta(q)
-        fp.rows.append((row[0], xi, eta, (gamma) % (2*np.pi)))
+        fp.rows.append((row[0], xi, eta, (gamma) % (2 * np.pi)))
     return fp
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--context-dir', default='context/')
-    parser.add_argument('--absolute', action='store_true', help=
-                        "Work around for relative paths bug in sotodlib ...")
-    parser.add_argument('export_dirs', nargs='+',
-                        help="Directories to search for HDF data files.")
+    parser.add_argument("--context-dir", default="context/")
+    parser.add_argument(
+        "--absolute",
+        action="store_true",
+        help="Work around for relative paths bug in sotodlib ...",
+    )
+    parser.add_argument(
+        "export_dirs", nargs="+", help="Directories to search for HDF data files."
+    )
 
     args = parser.parse_args()
 
     if not os.path.exists(args.context_dir):
         os.makedirs(args.context_dir, exist_ok=True)
 
-    tel_info_cachefile = os.path.join(args.context_dir, 'tels.h5')
-    
+    tel_info_cachefile = os.path.join(args.context_dir, "tels.h5")
+
     obsfiledb = metadata.ObsFileDb()
     obsdb = metadata.ObsDb()
-    obsdb.add_obs_columns(['timestamp float',
-                           'duration float',
-                           'start_time float',
-                           'stop_time float',
-                           'telescope string',
-                           'tel_tube string',
-                           'wafer_slots string',
-                           'target string',
-                           'toast_obs_name string',
-                           'toast_obs_uid string',
-    ])
+    obsdb.add_obs_columns(
+        [
+            "timestamp float",
+            "duration float",
+            "start_time float",
+            "stop_time float",
+            "telescope string",
+            "tel_tube string",
+            "wafer_slots string",
+            "target string",
+            "toast_obs_name string",
+            "toast_obs_uid string",
+        ]
+    )
 
     detsets = {}
 
     # Loop over data dirs...
     for export_dir in args.export_dirs:
-        files = glob.glob(os.path.join(export_dir, 'data', '*h5'))
-        print(f'Found {len(files)} files in {export_dir}')
+        files = glob.glob(os.path.join(export_dir, "data", "*h5"))
+        print(f"Found {len(files)} files in {export_dir}")
 
         path = export_dir
         practical_path = path
-        if not path.startswith('/'):
-            if args.context_dir.startswith('/'):
+        if not path.startswith("/"):
+            if args.context_dir.startswith("/"):
                 practical_path = os.path.abspath(path)
             else:
                 practical_path = os.path.relpath(path, args.context_dir)
 
         for filename in files:
-            with h5py.File(filename, 'r') as h:
+            with h5py.File(filename, "r") as h:
                 detdb = extract_detdb(h, db=None)
                 obs_info = extract_obs_info(h)
 
             # Convert detdb to ResultSet
             props = detdb.props()
-            props.keys[props.keys.index('det_id_')] = 'det_id'  # hotwire
+            props.keys[props.keys.index("det_id_")] = "det_id"  # hotwire
 
             # Figure out a couple more things ...
-            tel_type = props['tel_type'][0]
-            wafers = set(props['wafer_slot'])
+            tel_type = props["tel_type"][0]
+            wafers = set(props["wafer_slot"])
             telescope, tube, slot_mask = guess_tube(tel_type, wafers)
             print(slot_mask)
-            bands = list(set(props['band']))
+            bands = list(set(props["band"]))
 
             # Merge in that telescope name
-            props.merge(metadata.ResultSet(
-                keys=['telescope'], src=[(telescope,)] * len(props)))
+            props.merge(
+                metadata.ResultSet(keys=["telescope"], src=[(telescope,)] * len(props))
+            )
 
-            obs_info.update({'telescope': telescope,
-                             'tel_tube': tube,
-                             'wafer_slots': ','.join(wafers),
-                             })
+            obs_info.update(
+                {
+                    "telescope": telescope,
+                    "tel_tube": tube,
+                    "wafer_slots": ",".join(wafers),
+                }
+            )
 
             # In this format, all dets for the set of wafers and a
             # single band are stored in one file.  Create that detset
             # name from the list of wafers + band(s).
-            detset = '_'.join(sorted(list(wafers)) + sorted(list(bands)))
+            detset = "_".join(sorted(list(wafers)) + sorted(list(bands)))
             if detset not in detsets:
                 fp = detdb_to_focalplane(detdb)
                 detsets[detset] = [props, fp]
-                obsfiledb.add_detset(detset, props['readout_id'])
+                obsfiledb.add_detset(detset, props["readout_id"])
 
-            assert(len(bands) == 1)
+            assert len(bands) == 1
             obs_id = f'{int(obs_info["timestamp"])}_{tube}_{bands[0]}_{slot_mask}'
-            obsfiledb.add_obsfile(os.path.join(practical_path, 'data',
-                                               os.path.split(filename)[1]),
-                                  obs_id, detset, 0, 1)
+            obsfiledb.add_obsfile(
+                os.path.join(practical_path, "data", os.path.split(filename)[1]),
+                obs_id,
+                detset,
+                0,
+                1,
+            )
             # obs info
             obsdb.update_obs(obs_id, obs_info)
-            print(f'  added {obs_id}')
+            print(f"  added {obs_id}")
 
-
-    #detdb.to_file(f'{args.context_dir}/detdb.sqlite')
-    obsdb.to_file(f'{args.context_dir}/obsdb.sqlite')
-    obsfiledb.to_file(f'{args.context_dir}/obsfiledb.sqlite')
-
+    # detdb.to_file(f'{args.context_dir}/detdb.sqlite')
+    obsdb.to_file(f"{args.context_dir}/obsdb.sqlite")
+    obsfiledb.to_file(f"{args.context_dir}/obsfiledb.sqlite")
 
     #
     # metadata: det_info & focalplane
     #
 
     scheme = metadata.ManifestScheme()
-    scheme.add_exact_match('dets:detset')
-    scheme.add_data_field('dataset')
+    scheme.add_exact_match("dets:detset")
+    scheme.add_data_field("dataset")
     db1 = metadata.ManifestDb(scheme=scheme)
 
     scheme = metadata.ManifestScheme()
-    scheme.add_exact_match('dets:detset')
-    scheme.add_data_field('dataset')
+    scheme.add_exact_match("dets:detset")
+    scheme.add_data_field("dataset")
     db2 = metadata.ManifestDb(scheme=scheme)
 
     for detset, (props, fp) in detsets.items():
-        key = 'dets_' + detset
-        props.keys = ['dets:' + k for k in props.keys]
-        write_dataset(props, f'{args.context_dir}/metadata.h5', key, overwrite=True)
-        db1.add_entry({'dets:detset': detset, 'dataset': key},
-                      filename='metadata.h5')
+        key = "dets_" + detset
+        props.keys = ["dets:" + k for k in props.keys]
+        write_dataset(props, f"{args.context_dir}/metadata.h5", key, overwrite=True)
+        db1.add_entry({"dets:detset": detset, "dataset": key}, filename="metadata.h5")
 
-        key = 'focalplane_' + detset
-        write_dataset(fp, f'{args.context_dir}/metadata.h5', key, overwrite=True)
-        db2.add_entry({'dets:detset': detset, 'dataset': key},
-                      filename='metadata.h5')
-        
+        key = "focalplane_" + detset
+        write_dataset(fp, f"{args.context_dir}/metadata.h5", key, overwrite=True)
+        db2.add_entry({"dets:detset": detset, "dataset": key}, filename="metadata.h5")
 
-    db1.to_file(f'{args.context_dir}/det_info.sqlite')
-    db2.to_file(f'{args.context_dir}/focalplane.sqlite')
+    db1.to_file(f"{args.context_dir}/det_info.sqlite")
+    db2.to_file(f"{args.context_dir}/focalplane.sqlite")
 
     # And the context.yaml!
     context = {
-        'tags': {'metadata_lib': './'},
-        'imports': ['sotodlib.io.metadata'],
-        'obsfiledb': '{metadata_lib}/obsfiledb.sqlite',
+        "tags": {"metadata_lib": "./"},
+        "imports": ["sotodlib.io.metadata"],
+        "obsfiledb": "{metadata_lib}/obsfiledb.sqlite",
         #'detdb': '{metadata_lib}/detdb.sqlite',
-        'obsdb': '{metadata_lib}/obsdb.sqlite',
-        'obs_loader_type': 'toast3-hdf',
-        'obs_colon_tags': ['wafer_slot', 'band'],
-        'metadata': [
-            {'db': "{metadata_lib}/det_info.sqlite",
-             'det_info': True},
-            {'db': "{metadata_lib}/focalplane.sqlite",
-             'name': "focal_plane"}]
-        }
+        "obsdb": "{metadata_lib}/obsdb.sqlite",
+        "obs_loader_type": "toast3-hdf",
+        "obs_colon_tags": ["wafer_slot", "band"],
+        "metadata": [
+            {"db": "{metadata_lib}/det_info.sqlite", "det_info": True},
+            {"db": "{metadata_lib}/focalplane.sqlite", "name": "focal_plane"},
+        ],
+    }
 
     if args.absolute:
-        context['tags']['metadata_lib'] = args.context_dir
+        context["tags"]["metadata_lib"] = args.context_dir
 
-    open(f'{args.context_dir}/context.yaml', 'w').write(yaml.dump(context, sort_keys=False))
+    open(f"{args.context_dir}/context.yaml", "w").write(
+        yaml.dump(context, sort_keys=False)
+    )
