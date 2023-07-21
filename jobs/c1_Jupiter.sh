cdbc945b03aa639097a6bc87822cff7112085574
Kolen Cheung <christian.kolen@gmail.com>
2023-07-21 20:50:11 +0100

    add jobs/c1_Jupiter.*

---
+++ jobs/c1_Jupiter.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# helpers ##############################################################
+
+COLUMNS=72
+
+print_double_line() {
+	eval printf %.0s= '{1..'"${COLUMNS}"\}
+	echo
+}
+
+print_line() {
+	eval printf %.0s- '{1..'"${COLUMNS}"\}
+	echo
+}
+
+########################################################################
+print_double_line
+echo "Unpacking environment..."
+tar -xzf pmpm-20230718-Linux-x86_64-OpenMPI.tar.gz -C /tmp
+. /tmp/pmpm-20230718/bin/activate /tmp/pmpm-20230718
+print_line
+echo "Environment is available at:"
+which python
+
+print_double_line
+echo "Running run_planet_sims_pipeline.py..."
+./run_planet_sims_pipeline.py
